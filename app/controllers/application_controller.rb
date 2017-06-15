780407ad3c71d76e303f7a3596ea724930aef9d9
Brian Dunn <brian@danceoftehheart.com>
2017-06-15 16:04:18 +0000

    Set up root route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def hello
+    render html: "Once more, dear friends, into the breach! Thursday June 15, 2017"
 end
