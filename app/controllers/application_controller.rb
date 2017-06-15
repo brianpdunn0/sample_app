75c6f902b10eb5a3e6d5dc76335560d1736b4e1c
Brian Dunn <brian@danceoftehheart.com>
2017-06-15 16:09:22 +0000

    Trivial change to drive Heroku workflow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "Once more, dear friends, into the breach! Thursday June 15, 2017"
+    render html: "Once more, dear friends, into the breach!!! Thursday June 15, 2017"
 end
